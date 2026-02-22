e794e09f62589c1067ea9c9264606e4cf43a0572
salauddinahmedsr-cyber <salauddinahmedsr@gmail.com>
2026-02-22 12:03:52 +0600

    render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Install python dependencies
 pip install -r requirements.txt
+
+# Install Playwright browser ONLY (No sudo/deps)
 playwright install chromium
-playwright install-deps
